754449017c6e7b0d2aa38f8a1807b6419a0062a6
Zdenek Topic <zdenek@zdnkt.com>
2018-10-11 21:38:22 +0200

    Remove Worker requirement from FilesystemType methods

--- Sources/VaporFilesystem/FilesystemManager.swift
+++ Sources/VaporFilesystem/FilesystemManager.swift
@@ -6,16 +6,13 @@ public class FilesystemManager: Filesystem {
     public let disks: [DiskIdentifier: FilesystemAdapter]
     public let `default`: DiskIdentifier
     
-    public let worker: Worker
-    
     public init(disks: [DiskIdentifier: FilesystemAdapter], default: DiskIdentifier, on worker: Worker) throws {
         self.disks = disks
         self.default = `default`
-        self.worker = worker
         
         #warning("TODO: throw error if it is not present")
         let defaultAdapter = disks[`default`]!
-        super.init(adapter: defaultAdapter)
+        super.init(adapter: defaultAdapter, on: worker)
     }
     
 }
@@ -25,7 +22,7 @@ extension FilesystemManager: FilesystemManaging {
     public func use(_ id: DiskIdentifier) throws -> FilesystemType {
         #warning("TODO: throw error if it is not present")
         let adapter = disks[id]!
-        return Filesystem(adapter: adapter)
+        return Filesystem(adapter: adapter, on: worker)
     }
     
 }
