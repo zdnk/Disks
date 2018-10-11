d36299b51629e034709f773f8965fb1f4eb93281
Zdenek Topic <zdenek@zdnkt.com>
2018-10-11 21:28:19 +0200

    Fix path prefixing and file updating for local adapter

--- Sources/VaporFilesystem/Adapters/LocalAdapter.swift
+++ Sources/VaporFilesystem/Adapters/LocalAdapter.swift
@@ -19,16 +19,21 @@ public final class LocalAdapter: FilesystemAdapter {
         self.root = root
     }
     
+    public func applyPathPrefix(to path: String) -> String {
+        let prefixed = Filesystem.applyPathPrefix(self.root, to: path)
+        return URL(fileURLWithPath: prefixed).absoluteString
+    }
+    
     public func has(file: String, on worker: Worker, options: FileOptions?) -> EventLoopFuture<Bool> {
         return worker.eventLoop.submit { () -> Bool in
-            let path = Filesystem.applyPathPrefix(self.root, to: file)
+            let path = self.applyPathPrefix(to: file)
             return self.fileManager.fileExists(atPath: path)
         }
     }
     
     public func read(file: String, on worker: Worker, options: FileOptions?) -> EventLoopFuture<Data> {
         return worker.eventLoop.submit { () -> Data in
-            let path = Filesystem.applyPathPrefix(self.root, to: file)
+            let path = self.applyPathPrefix(to: file)
             guard let data = self.fileManager.contents(atPath: path) else {
                 throw LocalFilesystemError.fileNotFound
             }
@@ -44,7 +49,7 @@ public final class LocalAdapter: FilesystemAdapter {
     
     public func metadata(of file: String, on worker: Worker, options: FileOptions?) -> EventLoopFuture<FileMetadata> {
         return worker.eventLoop.submit { () -> [FileAttributeKey: Any] in
-            let path = Filesystem.applyPathPrefix(self.root, to: file)
+            let path = self.applyPathPrefix(to: file)
             return try self.fileManager.attributesOfItem(atPath: path)
             }.map { attributes -> FileMetadata in
                 var meta: FileMetadata = [:]
@@ -100,7 +105,7 @@ public final class LocalAdapter: FilesystemAdapter {
     
     public func write(data: Data, to file: String, on worker: Worker, options: FileOptions?) -> EventLoopFuture<()> {
         return worker.eventLoop.submit {
-            let path = Filesystem.applyPathPrefix(self.root, to: file)
+            let path = self.applyPathPrefix(to: file)
             guard self.fileManager.createFile(atPath: path, contents: data, attributes: nil) else {
                 throw LocalFilesystemError.fileCreationFailed
             }
@@ -109,18 +114,35 @@ public final class LocalAdapter: FilesystemAdapter {
     
     public func update(data: Data, to file: String, on worker: Worker, options: FileOptions?) -> EventLoopFuture<()> {
         return worker.eventLoop.submit {
-            let path = Filesystem.applyPathPrefix(self.root, to: file)
-            try self.fileManager.removeItem(atPath: path)
-            guard self.fileManager.createFile(atPath: file, contents: data, attributes: nil) else {
-                throw LocalFilesystemError.fileUpdateFailed
-            }
+            let path = self.applyPathPrefix(to: file)
+            let fileURL = URL(fileURLWithPath: path)
+            
+            #warning("TODO: check if file exists")
+            
+            // Saving to temp file
+            let tempUUID = UUID().uuidString
+            let tempURL = URL(fileURLWithPath: NSTemporaryDirectory())
+                .appendingPathComponent(tempUUID)
+            
+            self.fileManager.createFile(
+                atPath: tempURL.absoluteString,
+                contents: data,
+                attributes: nil
+            )
+            
+            // Replacing old one
+            _ = try self.fileManager.replaceItemAt(fileURL, withItemAt: tempURL, backupItemName: nil, options: [])
+            
+            // Remove temp file
+            try self.fileManager.removeItem(at: tempURL)
         }
     }
     
     public func rename(file: String, to newName: String, on worker: Worker, options: FileOptions?) -> EventLoopFuture<()> {
         return worker.eventLoop.submit {
-            let path = Filesystem.applyPathPrefix(self.root, to: file)
-            let dir = URL(string: path)!.deletingLastPathComponent()
+            let path = self.applyPathPrefix(to: file)
+            let fileURL = URL(fileURLWithPath: path)
+            let dir = fileURL.deletingLastPathComponent()
             let newURL = dir.appendingPathComponent(newName, isDirectory: false)
             let newPath = newURL.absoluteString
             try self.fileManager.moveItem(atPath: path, toPath: newPath)
@@ -129,29 +151,29 @@ public final class LocalAdapter: FilesystemAdapter {
     
     public func copy(file: String, to newFile: String, on worker: Worker, options: FileOptions?) -> EventLoopFuture<()> {
         return worker.eventLoop.submit {
-            let path = Filesystem.applyPathPrefix(self.root, to: file)
-            let newPath = Filesystem.applyPathPrefix(self.root, to: newFile)
+            let path = self.applyPathPrefix(to: file)
+            let newPath = self.applyPathPrefix(to: file)
             try self.fileManager.copyItem(atPath: path, toPath: newPath)
         }
     }
     
     public func delete(file: String, on worker: Worker, options: FileOptions?) -> EventLoopFuture<()> {
         return worker.eventLoop.submit {
-            let path = Filesystem.applyPathPrefix(self.root, to: file)
+            let path = self.applyPathPrefix(to: file)
             try self.fileManager.removeItem(atPath: path)
         }
     }
     
     public func delete(directory: String, on worker: Worker, options: FileOptions?) -> EventLoopFuture<()> {
         return worker.eventLoop.submit {
-            let path = Filesystem.applyPathPrefix(self.root, to: directory)
+            let path = self.applyPathPrefix(to: directory)
             try self.fileManager.removeItem(atPath: path)
         }
     }
     
     public func create(directory: String, on worker: Worker, options: FileOptions?) -> EventLoopFuture<()> {
         return worker.eventLoop.submit {
-            let path = Filesystem.applyPathPrefix(self.root, to: directory)
+            let path = self.applyPathPrefix(to: directory)
             try self.fileManager.createDirectory(atPath: path, withIntermediateDirectories: true, attributes: nil)
         }
     }
