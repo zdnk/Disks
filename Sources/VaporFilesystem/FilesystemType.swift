eb6ddbb058a388376534f5f46a413aec765fd8e9
Zdenek Topic <zdenek@zdnkt.com>
2018-10-11 21:44:58 +0200

    Make all options optional argument by extension on FilesystemType

--- Sources/VaporFilesystem/FilesystemType.swift
+++ Sources/VaporFilesystem/FilesystemType.swift
@@ -33,27 +33,66 @@ public protocol FilesystemType {
     
 }
 
-//extension FilesystemType {
-//    
-//    func has(file: String) -> Future<Bool>
-//    func read(file: String) -> Future<Data>
-//    func listContents(of: String, recursive: Bool) -> Future<[String]>
-//    func metadata(of: String) -> Future<FileMetadata>
-//    func size(of: String) -> Future<Int>
-//    func mimetype(of: String) -> Future<String>
-//    func timestamp(of: String) -> Future<Date>
-//    func visibility(of: String) -> Future<FileVisibility>
-//    func write(data: Data, to: String) -> Future<()>
-//    //    func write(file: String, to: String) -> Future<()>
-//    func update(data: Data, to: String) -> Future<()>
-//    //    func update(file: String, to: String) -> Future<()>
-//    func put(data: Data, to: String) -> Future<()>
-//    //    func put(file: String, to: String) -> Future<()>
-//    func rename(file: String, to: String) -> Future<()>
-//    func copy(file: String, to: String) -> Future<()>
-//    func delete(file: String) -> Future<()>
-//    func delete(directory: String) -> Future<()>
-//    func create(directory: String) -> Future<()>
-//    func setVisibility(of: String, to: FileVisibility) -> Future<()>
-//    
-//}
+extension FilesystemType {
+    
+    func has(file: String) -> Future<Bool> {
+        return has(file: file, options: nil)
+    }
+    
+    func read(file: String) -> Future<Data> {
+        return read(file: file, options: nil)
+    }
+    
+    func listContents(of: String, recursive: Bool) -> Future<[String]> {
+        return listContents(of: of, recursive: recursive, options: nil)
+    }
+    
+    func metadata(of: String) -> Future<FileMetadata> {
+        return metadata(of: of, options: nil)
+    }
+    
+    func size(of: String) -> Future<Int> {
+        return size(of: of, options: nil)
+    }
+    
+    func mimetype(of: String) -> Future<String> {
+        return mimetype(of: of, options: nil)
+    }
+    
+    func timestamp(of: String) -> Future<Date> {
+        return timestamp(of: of, options: nil)
+    }
+    
+    func write(data: Data, to: String) -> Future<()> {
+        return write(data: data, to: to, options: nil)
+    }
+    
+    func update(data: Data, to: String) -> Future<()> {
+        return update(data: data, to: to, options: nil)
+    }
+    
+    func put(data: Data, to: String) -> Future<()> {
+        return put(data: data, to: to, options: nil)
+    }
+    
+    func rename(file: String, to: String) -> Future<()> {
+        return rename(file: file, to: to, options: nil)
+    }
+    
+    func copy(file: String, to: String) -> Future<()> {
+        return copy(file: file, to: to, options: nil)
+    }
+    
+    func delete(file: String) -> Future<()> {
+        return delete(file: file, options: nil)
+    }
+    
+    func delete(directory: String) -> Future<()> {
+        return delete(directory: directory, options: nil)
+    }
+    
+    func create(directory: String) -> Future<()> {
+        return create(directory: directory, options: nil)
+    }
+    
+}
