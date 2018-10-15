00f87b9bcebbea1b25f13715193f1557a9a59762
Zdenek Topic <zdenek@zdnkt.com>
2018-10-15 18:32:40 +0200

    Implement getter and setter for basic metadata

--- Sources/VaporFilesystem/Filesystem/FileMetadata.swift
+++ Sources/VaporFilesystem/Filesystem/FileMetadata.swift
@@ -23,3 +23,34 @@ public struct FileMetadata: KeyValueStoring {
     public var storage: [FileMetadataKey: Any] = [:]
     
 }
+
+public extension FileMetadata {
+    
+    var creationDate: Date? {
+        get {
+            return try! get(.creationDate, as: Date.self)
+        }
+        set {
+            set(key: .creationDate, to: newValue)
+        }
+    }
+    
+    var modificationDate: Date? {
+        get {
+            return try! get(.modificationDate, as: Date.self)
+        }
+        set {
+            set(key: .modificationDate, to: newValue)
+        }
+    }
+    
+    var size: Int? {
+        get {
+            return try! get(.size, as: Int.self)
+        }
+        set {
+            set(key: .size, to: newValue)
+        }
+    }
+    
+}
