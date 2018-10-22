5baec68e7e71be56a04e21534cf12efa2e967d3a
Zdenek Topic <zdenek@zdnkt.com>
2018-10-22 21:42:44 +0200

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         
-        .package(url: "https://github.com/zdnk/S3.git", .branch("s3-copy-file")),
+        .package(url: "https://github.com/zdnk/S3.git", .branch("master")),
 //        .package(path: "../S3"),
     ],
     targets: [
